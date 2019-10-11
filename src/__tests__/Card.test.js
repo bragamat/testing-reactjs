eff093d0e28a99e1b994cb783a3667a723fc82c0
Mateus Braga <mateuscbraga@gmail.com>
2019-10-10 23:54:09 -0300

    writing the first tests

--- src/__tests__/Card.test.js
+++ src/__tests__/Card.test.js
@@ -3,7 +3,27 @@ import { shallow } from 'enzyme';
 
 import Card from '../components/Card/index';
 
-// Render the component
-it('renders without crashing', () => {
-  shallow(<Card />)
-});
+describe('Card', () => {
+    
+    // Render the component
+    it('Card renders without crashing', () => {
+        shallow(<Card />)
+    });
+    it('should render card title', () => {
+        throw new Error
+    })
+    it('should render card description', () => {
+        throw new Error
+    })
+    it('shouldn\'t render if neither title or description given', () => {
+        throw new Error
+    })
+})
+
+describe('Card rendering', () => {
+    
+    it('should throws error if no prop', () => {
+        throw new Error
+    })
+})
+
